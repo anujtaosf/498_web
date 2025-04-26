5ddf9b5cfe5449cee36840c97d88287cc6ba1fda
Anu Tao <anuhea@umich.edu>
2025-04-26 04:00:19 -0400

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/498_web/', // <-- your repo name
   plugins: [react()],
-  base: '/579_final_proj/', // or '/myapp/' if deploying to a subfolder
-});
+})
